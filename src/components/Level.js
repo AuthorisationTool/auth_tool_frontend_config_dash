9d33535a51c3b9a2a48bda5a7bf259322d99208c
bychis <bychis6@gmail.com>
2020-06-30 02:20:53 +0100

    manipulating the level component to fetch constraints and put them into custom cards via mapping loop

--- src/components/Level.js
+++ src/components/Level.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import {getConstraintsAll,createConstraint,deleteConstraint} from '../services/ConstraintService';
 import { useState, useEffect } from 'react';
 {/*
     props include : [ props.roledid, props.levelid ]
@@ -7,29 +8,31 @@ export default function Level(props) {
 
     const [constraintList, setconstraintList] = useState([]);
     const [reupload, setreupload] = useState(false);
-    const [roleid, setroleid] = useState(props.roleid);
 
-    const fetchLevelList =  () => {
-        axios.get(`http://192.168.1.104/policy/role/${props.roleid}/level/${props.levelid}/constraint`).then(res => {
-        console.log(res);
-        setlevelList(res.data)});
-      }
+    const fetchConstraintList =  () => {
+        getConstraintsAll().then(res => {
+            console.log(res);
+            setconstraintList(res.data);
+        })
+    } 
+    
+    useEffect(() => {
+        fetchConstraintList();
+    },[reupload]);
 
     useEffect(() => {
-        fetchLevelList()
-    }, [roleid]);
-
-    useEffect(() => {
-        fetchLevelList()
+        fetchConstraintList();
     }, []);
 
 
     return (
         <div>
           <Paper elevation={3}>
+                <Typography variant="h3">{`LEVEL ID: ${props.levelid}`}</Typography>
+                <Typography variant="h3">{`ACCESS CONTROL MODEL : ${props.model}`}</Typography>
+                <div>
 
-            
-
+                </div>
           </Paper>
         </div>
     );
--- src/services/ConstraintService.js
+++ src/services/ConstraintService.js
@@ -1,6 +1,19 @@
 import http from '../http-common'
 
-export function getConstraintsAll(roleid,levelid){
-    http.get(`/role/${roleid}/level/${levelid}`);
+export async function getConstraintsAll(roleid,levelid){
+    await http.get(`/role/${roleid}/level/${levelid}`);
 }
 
+export async function createConstraint(roleid,levelid,constraint){
+    await http.post(`/role/${roleid}/level/${levelid}/constraint`,{
+        "id": `${constraint.id}`,
+        "permission_or_action": `${constraint.permission_or_action}`,
+        "constraint_type": `${constraint.type}`,
+        "specific_type": `${constraint.name}`,
+        "arg": `${constraint.arg}`
+    });
+}
+
+export async function deleteConstraint(roleid,levelid,id){
+    await http.delete(`/role/${roleid}/level/${levelid}/constraint/${id}`);
+}
